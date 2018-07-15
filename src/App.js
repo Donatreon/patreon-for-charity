94621778d483da5e87c98f877d1b83f91119533a
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-07-14 23:15:47 -0500

    working on CharityButtons image sizing

--- src/App.js
+++ src/App.js
@@ -3,24 +3,25 @@ import { Route } from 'react-router-dom';
 
 import Home from 'views/Home';
 import CreateOrg from 'views/CreateOrg';
+import Profile from 'views/Profile';
 import SignIn from 'views/SignIn';
 
 import NavBar from './components/NavBar';
 
 import 'normalize.css/normalize.css';
 import './App.css';
-import data from './fake-data.json';
 
 class App extends Component {
-  state = {
-    orgs: data.orgs
-  };
+  // state = {
+  //   orgs: data.orgs
+  // };
   render() {
     return (
       <div className="App">
         <NavBar />
         <Route exact path="/" component={Home} />
         <Route path="/create-org" component={CreateOrg} />
+        <Route path="/profile" component={Profile} />
         <Route path="/signin" component={SignIn} />
       </div>
     );
--- src/components/CharityButton.js
+++ src/components/CharityButton.js
@@ -1,16 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import GridList from '@material-ui/core/GridList';
 import DonateButton from './DonateButton';
 
-const CharityButton = () => (
-  <div>
-    <div>
-      Title
-    </div>
-    <div>
-      Image
-    </div>
-    <DonateButton />
-  </div>
-)
+const styles = theme => ({
+  root: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-around',
+    overflow: 'hidden',
+    backgroundColor: theme.palette.background.paper,
+  },
+});
 
-export default CharityButton;
+const CharityButton = ({ name, image, classes }) =>
+  (
+    <div className={classes.root}>
+      <div>
+        {name}
+      </div>
+      <img src={image} alt={name} className="logos" />
+      <DonateButton />
+    </div>
+  )
+
+CharityButton.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(CharityButton);
