5ab950c53d1f6a377bd7fa883fa831805dddf8d6
ndrewr <uncleoptimus@gmail.com>
2018-07-14 19:15:15 -0500

    Add Route components; Create View stub components.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import React, { Component } from 'react';
+
+import {
+  // BrowserRouter as Router,
+  Route,
+  // Link
+} from 'react-router-dom'
+
 import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/NavBar';
+
+import Home from 'views/Home'
+import CreateOrg from 'views/CreateOrg'
+import OrgDetails from 'views/OrgDetails'
+import SignUp from 'views/SignUp'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+
+        <Route exact path="/" component={Home}/>
+        <Route path="/create-org" component={CreateOrg}/>
+        <Route path="/org-details" component={OrgDetails}/>
+        <Route path="/signup" component={SignUp}/>
       </div>
     );
   }
