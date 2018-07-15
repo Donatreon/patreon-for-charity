8affabec32a75162b55688bd9d4c2fb9fbb1ae55
JessicaSung <18690072+JessicaSung@users.noreply.github.com>
2018-07-15 01:35:16 -0500

    added to nav bar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,10 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-// import IconButton from '@material-ui/core/IconButton';
+import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
+
+import HomeIcon from './HomeIcon';
 
 const styles = {
   root: {
@@ -29,13 +31,13 @@ function NavBar(props) {
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+            <HomeIcon href="" />
+          </IconButton>
           <Typography variant="title" color="inherit" className={classes.flex}>
             Patreon For Charity
           </Typography>
-          {/* <NavLink to="/signin"> */}
-
-          <Button color="inherit" href="signin">Sign In</Button>
-          {/* </NavLink> */}
+          <Button color="inherit" href="#signin">Sign In</Button>
         </Toolbar>
       </AppBar>
     </div >
